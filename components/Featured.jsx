d56d73471aee3c004a7d386400df87c7e9ed5293
Devendra Utekar <devendrautekar91@gmail.com>
2022-02-22 19:11:42 +0530

    Establish the connection between mongoDB with application.

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,36 +1,3 @@
-// import React from "react";
-// import Image from "next/image";
-// import styles from "../styles/Featured.module.css";
-
-// const Featured = () => {
-//   const images = ["/img/featured.jpg", "/img/featured1.jpg", "/img/bake.png"];
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.arrowContainer} style={{ left: 0 }}>
-//         <Image src="/img/arrowl.png" alt="" layout="fill" />
-//       </div>
-//       <div className={styles.wrapper}>
-//         {images.map((img, i) => (
-//           <div className={styles.imgContainer} key={i}>
-//             <Image
-//               src="/img/featured.jpg"
-//               alt=""
-//               layout="fill"
-//               objectFit="cover"
-//             />
-//           </div>
-//         ))}
-//       </div>
-
-//       <div className={styles.arrowContainer} style={{ right: 0 }}>
-//         <Image src="/img/arrowr.png" layout="fill" alt="" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Featured;
-
 import styles from "../styles/Featured.module.css";
 import Image from "next/image";
 import { useState } from "react";
