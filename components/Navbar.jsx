4f59a62494fe57f50945bbafacc5976217a670e8
Devendra Utekar <devendrautekar91@gmail.com>
2022-03-05 15:06:36 +0530

    Building paypal payment gateway.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { useSelector } from "react-redux";
 import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
   const quantity = useSelector((state) => state.cart.quantity);
@@ -27,17 +28,19 @@ const Navbar = () => {
             <li className={styles.listItem}>Contact</li>
           </ul>
         </div>
-        <div className={styles.item}>
-          <div className={styles.cart}>
-            <Image
-              src="/img/cart.png"
-              alt=""
-              width="30px"
-              height="30px"
-            ></Image>
-            <div className={styles.counter}>{quantity}</div>
+        <Link href="/cart">
+          <div className={styles.item}>
+            <div className={styles.cart}>
+              <Image
+                src="/img/cart.png"
+                alt=""
+                width="30px"
+                height="30px"
+              ></Image>
+              <div className={styles.counter}>{quantity}</div>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     </>
   );
